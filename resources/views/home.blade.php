691b62835560a699f0b80f97af54aec14b4346a4
Lukasz Grychowski <lg@mettrr.com>
2016-10-10 15:51:58 +0000

    ugly calendar added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,10 +75,12 @@
             <div class="form-group">
                 <button type="submit" class="btn btn-primary ">Add Meeting</button>
             </div>
-        
-        </form>
-        
-        
+            
+            
+         </form>
+         
+<iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=hello%40mettrr.com&amp;color=%231B887A&amp;ctz=Europe%2FLondon" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <!--<example></example>-->
         
         
         
